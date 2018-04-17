39364180f033a4906db5db22d5de1f408ae3f46c
huanglinzhi <hnyanlin19@163.com>
2018-04-17 17:39:07 +0800

    增加判断pc

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div id="app">
+  <div id="app" v-bind:style="styleObject">
     <div class="test">test</div>
   </div>
 </template>
 
 <script>
+import {isPc} from '@/Common';
+
 export default {
+  data(){
+    return {
+      styleObject:{}
+    }
+  },
   mounted(){
+    // 如果是pc 则限制最大宽度 750
+    if (isPc()) {
+      this.styleObject = {
+        maxWidth: '750px',
+      };
+    }
     axios.get('/api/test')
     .then(function (response) {
       console.log(response);
@@ -21,7 +34,9 @@ export default {
 
 <style lang="scss">
   @import './assets/css/base.scss';
-  
+  #app{
+    margin: 0 auto;
+  }
   .test {
     display: flex;
     width: 300px;
